f48f24bcf70ea5f9cc6dfe4e5f1bc0183bec8104
Robin Lindner <robin@deeprobin.de>
2020-10-11 22:10:18 +0200

    Added plugin gallery base

---
+++ src/components/PluginComponent.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    import { _ } from 'svelte-i18n';
+
+    export let name: string;
+    export let description: string;
+    export let version: string;
+    export let install: boolean;
+</script>
+
+<style lang="scss">
+    .plugin {
+        display: grid;
+        grid-template-columns: 80% 20%;
+        border-radius: 5px;
+        background-color: var(--theme-selection-grid-background);
+
+        & > div {
+            padding: 12px;
+        }
+    }
+
+    .action-button {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
+        font-weight: 900;
+        cursor: pointer;
+        transition: all 200ms;
+        
+        &[data-action=install] {
+            background-color: #2ecc71;
+
+            &:hover {
+                background-color: #27ae60;
+            }
+        }
+
+        &[data-action=uninstall] {
+            background-color: #e74c3c;
+
+            &:hover {
+                background-color: #c0392b;
+            }
+        }
+    }
+
+    .name {
+        font-weight: 600;
+    }
+
+    .version {
+        font-weight: 100;
+        font-size: 10pt;
+        color: #bdc3c7;
+
+        &::before {
+            content: "Version ";
+        }
+    }
+
+    .description {
+        margin-top: 4px;
+        font-weight: 100;
+    }
+
+    .text-container {
+        text-align: left;
+    }
+</style>
+
+<div class="plugin">
+    <div class="text-container">
+        <div>
+            <span class="name">{name}</span>
+            <span class="version">{version}</span>
+        </div>
+        <span class="description">{description}</span>
+    </div>
+    <div class="action-button" data-action="{install ? "install" : "uninstall"}">{install ? $_("plugins.action.install") : $_("plugins.action.uninstall")}</div>
+</div>
---
+++ src/pages/Plugins.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+    import PluginComponent from "../components/PluginComponent.svelte";
+    import { _ } from 'svelte-i18n';
+
+    enum PluginTab {
+        InstalledPlugins = 0,
+        PluginGallery = 1,
+    }
+
+    class PluginInfo {
+        constructor(
+            public name: string,
+            public description: string,
+            public version: string,
+            public installed: boolean
+        ) {}
+    }
+
+    let currentTab: PluginTab = PluginTab.InstalledPlugins;
+
+    function getPlugins(tab: PluginTab) {
+        switch (tab) {
+            case PluginTab.InstalledPlugins:
+                return [
+                    new PluginInfo(
+                        "Installed Plugin 1",
+                        "Lorem ipsum dolor sit amet.",
+                        "1.0.0",
+                        true
+                    ),
+                ];
+            case PluginTab.PluginGallery:
+                return [
+                    new PluginInfo(
+                        "Gallery Plugin 1",
+                        "Lorem ipsum dolor sit amet",
+                        "1.0.0",
+                        false
+                    ),
+                ];
+        }
+    }
+</script>
+
+<style type="text/scss">
+    h1 {
+        color: #f19066;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    .flex-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    .page-wrapper {
+        width: 70%;
+    }
+
+    .gallery-selection {
+        display: grid;
+        width: 100%;
+        grid-template-columns: repeat(2, 50%);
+
+        div {
+            padding: 24px;
+            background-color: var(--theme-sidebar-background);
+            transition: all 240ms;
+            cursor: pointer;
+
+            &:hover,
+            &[aria-selected=true] {
+                background-color: var(--theme-sidebar-background-hover);
+            }
+        }
+    }
+
+    .plugin-overview {
+        width: 100%;
+    }
+</style>
+
+<h1>Plugins</h1>
+<div class="flex-wrapper">
+    <div class="page-wrapper">
+        <div class="gallery-selection">
+            <div
+                aria-selected={currentTab == PluginTab.InstalledPlugins}
+                on:click={() => (currentTab = PluginTab.InstalledPlugins)}>
+                {$_("plugins.gallery_type.installed")}
+            </div>
+            <div
+                aria-selected={currentTab == PluginTab.PluginGallery}
+                on:click={() => (currentTab = PluginTab.PluginGallery)}>
+                {$_("plugins.gallery_type.gallery")}
+            </div>
+        </div>
+        <div class="plugin-overview">
+            <div class="plugin-container">
+                {#each getPlugins(currentTab) as plugin}
+                    <PluginComponent
+                        name={plugin.name}
+                        description={plugin.description}
+                        version={plugin.version}
+                        install={!plugin.installed}/>
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
